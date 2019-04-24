346f961b40f490ee1e9127535961e5efc6a2e4bb
Andrew <andrew.panix@gmail.com>
2019-04-24 08:48:34 +0300

    init

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
 error_reporting(E_ALL);
 //Timezone
 date_default_timezone_set("UTC");
@@ -15,11 +13,13 @@ if ($_SERVER['REMOTE_ADDR'] == '127.0.0.1') {
     $debug = false;
 
 }
+defined('COMMON_PATH') or define('COMMON_PATH', __DIR__ . '/../shopium24.loc');
 defined('YII_DEBUG') or define('YII_DEBUG', $debug);
 defined('YII_ENV') or define('YII_ENV', $env);
 
-require(__DIR__ . '/../shopium24.loc/vendor/yiisoft/yii2/Yii.php');
-require(__DIR__ . '/../shopium24.loc/vendor/autoload.php');
+require __DIR__ . '/../shopium24.loc/vendor/autoload.php';
+require __DIR__ . '/../shopium24.loc/vendor/yiisoft/yii2/Yii.php';
+
 
 
 
@@ -28,9 +28,4 @@ $config = yii\helpers\ArrayHelper::merge(
     require __DIR__ . '/config/web.php'
 );
 
-//use yii\web\Application;
-use panix\engine\WebApplication;
-
-$app = new WebApplication($config);
-//Yii::setAlias('@bower', dirname(__DIR__) . '/vendor/bower-asset');
-$app->run();
+(new panix\engine\WebApplication($config))->run();
