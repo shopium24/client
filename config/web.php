f6b8327c98bdc4576dd8c0795ed981a41fea4ae4
Andrew <andrew.panix@gmail.com>
2019-04-30 01:48:53 +0300

    init

--- config/web.php
+++ config/web.php
@@ -4,21 +4,20 @@ Yii::setAlias('@client', dirname(__DIR__));
 
 $config = [
     'id' => 'client',
-    'basePath' => dirname(__DIR__).'/../shopium24.loc', //if in web dir
-	//'vendorPath' => dirname(__DIR__).'/../shopium24.loc/vendor',
+    'basePath' => dirname(__DIR__) . '/../shopium24.loc', //if in web dir
+    //'vendorPath' => dirname(__DIR__).'/../shopium24.loc/vendor',
     //'basePath' => dirname(__DIR__),
-    'runtimePath'=>'@client/runtime',
+    'runtimePath' => '@client/runtime',
     'components' => [
-        //'urlManager' => require(__DIR__ . '/urlManager.php'),
         'db' => [
             'class' => 'panix\engine\db\Connection',
             'dsn' => 'mysql:host=localhost;dbname=shopium24',
             'username' => 'root',
             'password' => '47228960panix',
-            'charset' => 'utf8mb4', //utf8_general_ci на utf8mb4_general_ci. FOR Emoji
+            'charset' => 'utf8mb4',
             'tablePrefix' => 'cms_',
             'serverStatusCache' => YII_DEBUG ? 0 : 3600,
-            'schemaCacheDuration' => YII_DEBUG ? 0 : 3600*24,
+            'schemaCacheDuration' => YII_DEBUG ? 0 : 3600 * 24,
             'enableSchemaCache' => true,
             'schemaCache' => 'cache',
 
