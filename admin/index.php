5258ee2587ea467ebbfac39f0bc2cec2d7e3fbd8
Andrew <andrew.panix@gmail.com>
2019-11-29 03:37:03 +0200

    init

--- admin/index.php
+++ admin/index.php
@@ -13,7 +13,7 @@ if ($_SERVER['REMOTE_ADDR'] == '127.0.0.1') {
     $debug = false;
 
 }
-defined('CORE_PATH') or define('CORE_PATH', __DIR__ . '/../../common');
+defined('COMMON_PATH') or define('COMMON_PATH', __DIR__ . '/../../common');
 defined('YII_DEBUG') or define('YII_DEBUG', $debug);
 defined('YII_ENV') or define('YII_ENV', $env);
 
