b5ef573ce09d0a8c21cc1a6326b506499a2b641f
Andrew <andrew.panix@gmail.com>
2019-11-29 02:21:57 +0200

    init

--- config/web.php
+++ config/web.php
@@ -1,5 +1,5 @@
 <?php
-Yii::setAlias('@core', CORE_PATH);
+
 $config = [
     'id' => 'client',
     'vendorPath' => CORE_PATH . '/vendor',
@@ -10,16 +10,10 @@ $config = [
     ],
     'components' => [
         'db' => [
-            'class' => 'panix\engine\db\Connection',
             'dsn' => 'mysql:host=localhost;dbname=yii2',
             'username' => 'root',
             'password' => '47228960panix',
-            'charset' => 'utf8mb4',
             'tablePrefix' => 'cms_',
-            'serverStatusCache' => YII_DEBUG ? 0 : 3600,
-            'schemaCacheDuration' => YII_DEBUG ? 0 : 3600 * 24,
-            'enableSchemaCache' => true,
-            'schemaCache' => 'cache',
 
         ],
         'view' => [
@@ -28,7 +22,9 @@ $config = [
             ],
         ],
     ],
-    'params' => require(CORE_PATH . '/config/params.php'),
+    'params' => yii\helpers\ArrayHelper::merge([
+        'plan_id' => 2,
+    ],require(CORE_PATH . '/config/params.php')),
 ];
 
 return $config;
